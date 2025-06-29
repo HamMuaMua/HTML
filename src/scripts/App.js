a52f30676476d8cd20b3af4c10956c9f0cefb319
HamMuaMua <ysy201051@163.com>
2025-06-29 12:18:11 +0800

    Update App.js

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,1 +1,24 @@
+import Navbar from '../components/Navbar.js';
+import Gallery from '../components/Gallery.js';
+// 其他组件导入...
 
+export default class App {
+    constructor() {
+        this.components = {
+            navbar: new Navbar(),
+            gallery: new Gallery()
+            // 其他组件实例...
+        };
+    }
+
+    init() {
+        // 渲染所有组件
+        Object.entries(this.components).forEach(([name, component]) => {
+            const container = document.getElementById(name);
+            if (container) {
+                container.innerHTML = component.render();
+                component.init();
+            }
+        });
+    }
+}
